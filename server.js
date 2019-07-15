22ea24849619d51c9de9a8ded4c1b97f6002d84e
Mitch MacKenzie <mitch@forestry.io>
2019-07-15 16:49:39 -0300

    Update server port

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.prepare()
       handle(req, res, parsedUrl)
     }
   })
-  .listen(3000, (err) => {
+  .listen(8080, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
